43e9a3114097181bba298cc3f5967f0025a40a57
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2021-04-29 22:31:38 +0100

    update api to latest hierarchy changes

--- hierarchy.cpp
+++ hierarchy.cpp
@@ -10,14 +10,10 @@
 namespace la {
   hy::model_t update(hy::model_t current, action_t action) {
     const auto initial_selection = [&] {
-      if (
-        current.interaction_.element_ == -1
-        && current.interaction_.selected_ == thh::handle_t()) {
+      if (current.interaction_.selected() == thh::handle_t()) {
         if (!current.root_.empty() && !current.entities_.empty()) {
-          current.interaction_.element_ = 0;
-          current.interaction_.selected_ =
-            current.root_[current.interaction_.element_];
-          current.interaction_.siblings_ = current.root_;
+          current.interaction_.select(
+            current.root_.front(), current.entities_, current.root_);
         }
         return true;
       }
@@ -30,37 +26,28 @@ namespace la {
           current.entities_.call(handle, [handle](auto& entity) {
             entity.name_ = std::string("entity_") + std::to_string(handle.id_);
           });
-          if (
-            current.interaction_.element_ == -1
-            && current.interaction_.selected_ == thh::handle_t()) {
+          if (current.interaction_.selected() == thh::handle_t()) {
             current.root_.push_back(handle);
-            current.interaction_.siblings_ =
-              hy::siblings(handle, current.entities_, current.root_);
           } else {
             hy::add_children(
-              current.interaction_.selected_, {handle}, current.entities_);
-            current.interaction_.siblings_ = hy::siblings(
-              current.interaction_.selected_, current.entities_, current.root_);
+              current.interaction_.selected(), {handle}, current.entities_);
           }
           return current;
         },
         [&](move_down_action_t) {
           if (!initial_selection()) {
-            hy::move_down(
-              current.interaction_, current.entities_, current.root_);
+            current.interaction_.move_down(current.entities_, current.root_);
           }
           return current;
         },
         [&](move_up_action_t) {
           if (!initial_selection()) {
-            hy::move_up(current.interaction_, current.entities_, current.root_);
+            current.interaction_.move_up(current.entities_, current.root_);
           }
           return current;
         },
         [&](deselect_action_t) {
-          current.interaction_.element_ = -1;
-          current.interaction_.selected_ = thh::handle_t();
-          current.interaction_.siblings_ = {};
+          current.interaction_.deselect();
           return current;
         }},
       action);
